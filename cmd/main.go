415769c8b781e7204e32a537adad3e17ee71ff4f
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2026-01-15 14:35:56 -0300

    constants in go

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 // https://go.dev/tour/list
-
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println(`Hello
@@ -41,8 +40,13 @@ func main() {
 	var xFH, xSH int = half(2)
 	fmt.Printf("If there are 2 oranges, and we share them... I will get %d orange and you will get %d orange.\n", xFH, xSH)
 
-	implicitTypeAndDeclaration := "This string was declared using the ':=' operator, which does not require typing the keyword 'var' nor its type."
+	implicitTypeAndDeclaration := "This string was declared using the ':=' operator, which does not require typing the keyword 'var' nor its type." // Cannot reassign value for this variable if its not an integer
+	// Cannot create an constant using ":=" operator
 	fmt.Println(implicitTypeAndDeclaration)
+
+	const helloWorld = "Olá Mundo!"
+	const olaMundo string = "Hello World!"
+	fmt.Println("Invoking value of two constants: " + helloWorld + " " + olaMundo)
 }
 
 // func keyword + name of the function + (varName type) + return type + block of code
